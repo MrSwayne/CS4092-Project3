2592807858a9171adbc957e72c3297a8be856210
MatthewBlake <mzb1998@gmail.com>
2017-03-27 16:19:53 +0100

    Update flightManager.java

--- flightManager.java
+++ flightManager.java
@@ -2,7 +2,7 @@ import javax.swing.*;
 import java.io.*;
 import java.util.*;
 
-public class flightManager
+public class FlightManager
 {
         public static ArrayList<ArrayList<String>> airportList = new ArrayList<ArrayList<String>>();
         public static ArrayList<ArrayList<String>> flightList = new ArrayList<ArrayList<String>>();
